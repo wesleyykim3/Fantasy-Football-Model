8cacd54ad94adffc625a9e2e573251be78b08481
wesleyykim3 <wesleyykim@gmail.com>
2024-08-24 00:38:31 -0700

    test

--- docs/script.js
+++ docs/script.js
@@ -1,31 +1,49 @@
 document.addEventListener('DOMContentLoaded', function() {
 
-    // Function to create a table from CSV data
+    // function to create a table from CSV data
     function createTableFromCSV(csvData, tableContainer) {
         const rows = csvData.split('\n');
         const table = document.createElement('table');
-        
-        rows.forEach(row => {
+    
+        const thead = document.createElement('thead');
+        const headerRow = document.createElement('tr');
+        const headers = rows[0].split(',');
+    
+        headers.forEach(header => {
+            const th = document.createElement('th');
+            th.textContent = header;
+            headerRow.appendChild(th);
+        });
+    
+        thead.appendChild(headerRow);
+        table.appendChild(thead);
+    
+        const tbody = document.createElement('tbody');
+    
+        rows.slice(1).forEach(row => { 
             const tr = document.createElement('tr');
             const cells = row.split(',');
-            
+    
             cells.forEach(cell => {
                 const td = document.createElement('td');
                 td.textContent = cell;
                 tr.appendChild(td);
             });
-            
-            table.appendChild(tr);
+    
+            tbody.appendChild(tr);
         });
-        console.log(table)
+    
+        table.appendChild(tbody);
         tableContainer.appendChild(table);
     }
 
+    // function to set page when button is pressed
     function setPage(page) {
         dispTable(page)
     }
     window.setPage = setPage
 
+    // function to display table
     function dispTable(page) {
         const table = document.getElementById('table');
         table.textContent = "";
@@ -34,6 +52,6 @@ document.addEventListener('DOMContentLoaded', function() {
         .then(data => createTableFromCSV(data, table));
     }
 
-    let page = 'all_players'
+    let page = 'tes'
     dispTable(page)
 });
